ccec380e2be8da30bcfa41910ebaad168069f39e
AsafKaravani <asaf.karavani1@gmail.com>
2018-12-13 07:31:19 +0200

    added - home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseComponent } from './components/base/base.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    NavComponent,
+    FooterComponent,
+    BodyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
