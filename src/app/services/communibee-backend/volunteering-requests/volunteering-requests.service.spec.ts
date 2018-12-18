0fae295debfb393443e761b89a97d016f2414d7f
bikov <bikov95@gmail.com>
2018-12-18 15:10:04 +0200

    services to backend calls added

--- src/app/services/communibee-backend/volunteering-requests/volunteering-requests.service.spec.ts
+++ src/app/services/communibee-backend/volunteering-requests/volunteering-requests.service.spec.ts
@@ -5,7 +5,7 @@ import { VolunteeringRequestsService } from './volunteering-requests.service';
 describe('VolunteeringEventsService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created ', () => {
+  it('should be created', () => {
     const service: VolunteeringRequestsService = TestBed.get(VolunteeringRequestsService);
     expect(service).toBeTruthy();
   });
