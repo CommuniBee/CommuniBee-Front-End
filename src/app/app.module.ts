2a7d32357949cd8a46fb362e93fbba3b479b5391
Or Bin <orbin50@gmail.com>
2019-01-07 23:20:22 +0200

    Added authentication interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +13,8 @@ import { ContactModalComponent } from './components/contact-modal/contact-modal.
 import { BrowseContentComponent } from './components/browse-content/browse-content.component';
 import { BrowseOpportunitiesComponent } from './components/browse-opportunities/browse-opportunities.component';
 import { BrowseVolunteersComponent } from './components/browse-volunteers/browse-volunteers.component';
-import {AuthService} from './services/communibee-backend/auth/auth.service';
+import {  AuthService } from './services/communibee-backend/auth/auth.service';
+import { AuthInterceptorService } from './services/auth-interceptor/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import {AuthService} from './services/communibee-backend/auth/auth.service';
     HttpClientModule,
   ],
   providers: [
-    AuthService
+    AuthService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true},
   ],
   bootstrap: [AppComponent],
 })
---
+++ src/app/services/auth-interceptor/auth-interceptor.service.ts
@@ -0,0 +1,15 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../communibee-backend/auth/auth.service';
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private auth: AuthService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const accessToken = this.auth.getAccessToken();
+    const copiedReq = req.clone({headers: req.headers.set('Authorization', `Bearer ${accessToken}`)});
+    return next.handle(copiedReq);
+  }
+}
