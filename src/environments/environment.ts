89d5ed82305b01cbc57c7b9b57c6c7e3624621bd
Or Bin <orbin50@gmail.com>
2019-01-07 23:38:57 +0200

    Added empty environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  auth0: {
+    clientId: '',
+    domain: '',
+    redirectUri: '',
+  }
 };
 
 /*
@@ -14,3 +19,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
