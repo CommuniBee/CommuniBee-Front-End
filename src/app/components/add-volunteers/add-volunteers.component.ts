66129d68556d479177bb2e1e4d122549e85c3e92
AsafKaravani <asaf.karavani1@gmail.com>
2019-01-17 08:04:13 +0200

    added - FormGroup and regions from server

--- src/app/components/add-volunteers/add-volunteers.component.ts
+++ src/app/components/add-volunteers/add-volunteers.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {RegionService} from '../../services/communibee-backend/region/region.service';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthService} from '../../services/communibee-backend/auth/auth.service';
+import {VolunteeringOffersService} from '../../services/communibee-backend/volunteering-offers/volunteering-offers.service';
 
 @Component({
   selector: 'app-add-volunteers',
@@ -6,10 +11,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-volunteers.component.scss']
 })
 export class AddVolunteersComponent implements OnInit {
+  public myForm: FormGroup;
+  regions: [string] = [] as any;
 
-  constructor() { }
+  constructor(private regionsSrv: RegionService,
+              private fb: FormBuilder,
+              private vltrOffer: VolunteeringOffersService,
+              private auth: AuthService,
+              private rotuer: Router) {
+    this.initForm();
+  }
 
   ngOnInit() {
+    this.regionsSrv.getAll().then( regions_res => {
+      this.regions =  regions_res as any;
+      this.initForm();
+    });
+  }
+
+  initForm() {
+    this.myForm = this.fb.group({
+      title: ['', Validators.required],
+      poc: this.fb.group({
+        name: ['', Validators.required],
+        phone: ['', Validators.required],
+        email: ['', Validators.required]
+      }),
+      numberOfVolunteers: ['', Validators.required],
+      availableContent: [''],
+      days: this.fb.group({
+        1: [false],
+        2: [false],
+        3: [false],
+        4: [false],
+        5: [false],
+        6: [false],
+        7: [false]
+      }),
+      regions: this.fb.array(this.regions.map(region => this.fb.control(false))),
+    });
+  }
+
+
+  sendData() {
+    console.log(this.myForm.value);
   }
 
 }
