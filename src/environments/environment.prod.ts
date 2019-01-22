7bc754bdfabf375c339e705f2add88c219081144
Or Bin <orbin50@gmail.com>
2019-01-22 09:32:40 +0200

    Fixed ng build for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,10 @@ export const environment = {
   auth0: {
     clientId: 'QYeOypEJmHtXWi5NLxjXkuvn6ry0e8vy',
     domain: 'asafkaravani.auth0.com',
-    redirectUri: 'http://localhost:4200/login',
+    redirectUri: 'http://localhost:4200/dashboard',
+    registerRedirectUri: 'http://localhost:4200/register?auth0_callback=true',
+  },
+  api: {
+    url: 'http://localhost:3000'
   }
 };
