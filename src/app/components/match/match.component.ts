ac02e0e4b04c4d98ba8e1883d05e1f320d65872f
Or Bin <orbin50@gmail.com>
2019-01-23 08:35:16 +0200

    Integrated requests template into MatchComponent html

--- src/app/components/match/match.component.ts
+++ src/app/components/match/match.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {VolunteeringRequestsService} from '../../services/communibee-backend/volunteering-requests/volunteering-requests.service';
 import {VolunteeringOffersService} from '../../services/communibee-backend/volunteering-offers/volunteering-offers.service';
 import {VolunteeringEventsService} from '../../services/communibee-backend/volunteering-events/volunteering-events.service';
+import {VolunteeringOffer} from '../../services/communibee-backend/volunteering-offers/volunteering-offer';
+import {VolunteeringRequest} from '../../services/communibee-backend/volunteering-requests/volunteering-request';
 
 @Component({
   selector: 'app-match',
@@ -10,11 +12,16 @@ import {VolunteeringEventsService} from '../../services/communibee-backend/volun
 })
 export class MatchComponent implements OnInit {
 
+  volunteeringOffers: VolunteeringOffer[] = [];
+  volunteeringRequests: VolunteeringRequest[] = [];
+
   constructor(private volunteeringRequestsService: VolunteeringRequestsService,
               private volunteeringOffersService: VolunteeringOffersService,
               private volunteeringEventsService: VolunteeringEventsService) { }
 
   ngOnInit() {
+    this.volunteeringRequestsService.getAll().then(requests => { this.volunteeringRequests = requests; });
+    this.volunteeringOffersService.getAll().then(offers => { this.volunteeringOffers = offers; });
   }
 
 }
