d91b8b054bd86150765f2645ed7c5731213a363a
Kesem <kesemdavid123@gmail.com>
2019-02-05 14:01:22 +0200

    generic-table basic usage

--- src/app/components/dashboard-tabs/dashboard-tabs.component.ts
+++ src/app/components/dashboard-tabs/dashboard-tabs.component.ts
@@ -1,15 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {VolunteeringEventsService} from '../../services/communibee-backend/volunteering-events/volunteering-events.service';
 
 @Component({
   selector: 'app-dashboard-tabs',
   templateUrl: './dashboard-tabs.component.html',
   styleUrls: ['./dashboard-tabs.component.css']
 })
-export class DashboardTabsComponent {
+export class DashboardTabsComponent implements OnInit {
   private selectedTab: number;
+  events: any;
 
-  constructor() {
+  constructor(private eventsService: VolunteeringEventsService) {
     this.selectedTab = 0;
+  }
+
+  async ngOnInit() {
+    this.events = [{kesem: 'kesem', david: 'david'}];
+    //this.events = await this.eventsService.getAll();
   }
 
   selectTab(index: number) {
