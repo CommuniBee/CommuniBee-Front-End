4d1e9526c5f0985c8838d14c7c83884e6b89fff0
Kesem <kesemdavid123@gmail.com>
2019-02-05 10:42:58 +0200

    pr fixes

--- src/app/components/dashboard-tabs/dashboard-tabs.component.ts
+++ src/app/components/dashboard-tabs/dashboard-tabs.component.ts
@@ -1,18 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-tabs',
   templateUrl: './dashboard-tabs.component.html',
   styleUrls: ['./dashboard-tabs.component.css']
 })
-export class DashboardTabsComponent implements OnInit {
+export class DashboardTabsComponent {
   private selectedTab: number;
 
   constructor() {
-
-  }
-
-  ngOnInit() {
     this.selectedTab = 0;
   }
 
