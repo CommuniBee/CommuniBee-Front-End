36022d3b28f186cbb9d86ca0759c4dc4ccd57167
abrabing <brabbing@gmail.com>
2019-02-05 10:35:11 +0200

    add organization to form

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -34,36 +34,4 @@ export class NavComponent implements OnInit {
     $('#navModalContentUpload').modal('toggle');
   }
 
-  /** onFileChange(event) {
-    const fileSize = event.srcElement.files[0].size;
-    const maxFileSize = 5e+6; // 5MB
-    if (fileSize > maxFileSize) {
-      this.fileError = 'File size is bigger then 5mb';
-    } else {
-      this.fileError = '';
-      const reader = new FileReader();
-      if (event.target.files && event.target.files.length) {
-        const [file] = event.target.files;
-        reader.readAsArrayBuffer(file);
-
-        reader.onload = () => {
-          $('#navFileBrowserLabel').html(file.name);
-          const base64File = ArrBuff.arrayBufferToBase64(reader.result);
-
-          this.content.title = $('#navContentTitle').val();
-          this.content.file = base64File;
-          this.content.category = $('#navContentCategory').val();
-          this.content.fileName = file.name;
-
-        };
-      }
-    }
-  }
-
-  uploadContent() {
-    this.contentSrv.create(this.content).then(contentRes => {
-      this.content = contentRes;
-    });
-  }*/
-
 }
