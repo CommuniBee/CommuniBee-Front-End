c2e37bdd136f6ed14ee7c0ec2eae7e01ffdec45e
Kesem <kesemdavid123@gmail.com>
2019-02-06 09:57:19 +0200

    for eden

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthService} from '../../services/communibee-backend/auth/auth.service';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor(public auth: AuthService) { }
-
-  ngOnInit() {
-  }
+export class HomeComponent {
 }
--- src/app/services/communibee-backend/volunteering-offers/volunteering-offers.service.ts
+++ src/app/services/communibee-backend/volunteering-offers/volunteering-offers.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@angular/core';
-import { BackendModelService } from '../basic-model.service';
-import { BackendHttpService } from '../backend-http.service';
-import { VolunteeringOffer, VolunteeringOfferModel } from './volunteering-offer';
+import {Injectable} from '@angular/core';
+import {BackendModelService} from '../basic-model.service';
+import {BackendHttpService} from '../backend-http.service';
+import {VolunteeringBaseModel, VolunteeringRequestOfferBase} from '../common-models/volunteering-request-offer-base';
 
 const path = 'volunteering-offers';
 
 @Injectable({
   providedIn: 'root',
 })
-export class VolunteeringOffersService extends BackendModelService<VolunteeringOffer, VolunteeringOfferModel> {
+export class VolunteeringOffersService extends BackendModelService<VolunteeringRequestOfferBase, VolunteeringBaseModel> {
 
   constructor(private backendHttpService: BackendHttpService) {
     super(path, backendHttpService);
