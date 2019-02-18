da5968e3eb8f5e5c866bbdb04c8218fad67783e7
Kesem <kesemdavid123@gmail.com>
2019-02-18 10:45:31 +0200

    fk merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {AuthService} from './services/communibee-backend/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +7,4 @@ import {AuthService} from './services/communibee-backend/auth/auth.service';
 })
 export class AppComponent {
   title = 'CommuniBee';
-
-  constructor(private authService: AuthService) {
-  }
 }
